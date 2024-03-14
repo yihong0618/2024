b8962fcfc392f6265ad9d54f20ed74f9f6c0c623
yihong0618 <zouzou0208@gmail.com>
2024-03-14 22:56:09 +0800

    feat: til

--- get_up.py
+++ get_up.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+from pathlib import Path
 import random
 
 import pendulum
@@ -9,9 +10,10 @@ from BingImageCreator import ImageGen
 from github import Github
 from openai import OpenAI
 from telebot.types import InputMediaPhoto
+from md2tgmd import escape
 
-# 1 real get up
-GET_UP_ISSUE_NUMBER = 1
+# 1 real get up #5 for test
+GET_UP_ISSUE_NUMBER = 5
 GET_UP_MESSAGE_TEMPLATE = "今天的起床时间是--{get_up_time}.\r\n\r\n 起床啦。\r\n\r\n 今天的一句诗:\r\n {sentence} \r\n"
 SENTENCE_API = "https://v1.jinrishici.com/all"
 DEFAULT_SENTENCE = (
@@ -22,6 +24,17 @@ if api_base := os.environ.get("OPENAI_API_BASE"):
     client = OpenAI(base_url=api_base, api_key=os.environ.get("OPENAI_API_KEY"))
 else:
     client = OpenAI()
+
+
+def get_all_til_knowledge_file():
+    til_dir = Path(os.environ.get("MORNING_REPO_NAME"))
+    today_dir = random.choice(list(til_dir.iterdir()))
+    md_files = []
+    for root, _, files in os.walk(today_dir):
+        for file in files:
+            if file.endswith(".md"):
+                md_files.append(os.path.join(root, file))
+    return md_files
 
 
 def login(token):
@@ -67,12 +80,15 @@ def make_pic_and_save(sentence, bing_cookie):
     # for bing image when dall-e3 open drop this function
     i = ImageGen(bing_cookie)
     prompt = f"revise `{sentence}` to a DALL-E prompt"
-    completion = client.chat.completions.create(
-        messages=[{"role": "user", "content": prompt}],
-        model="gpt-4-1106-preview",
-    )
-    sentence = completion.choices[0].message.content.encode("utf8").decode()
-    print(f"revies: {sentence}")
+    try:
+        completion = client.chat.completions.create(
+            messages=[{"role": "user", "content": prompt}],
+            model="gpt-4-1106-preview",
+        )
+        sentence = completion.choices[0].message.content.encode("utf8").decode()
+        print(f"revies: {sentence}")
+    except:
+        print("revise sentence wrong")
 
     images = i.get_images(sentence)
     now = pendulum.now()
@@ -90,8 +106,10 @@ def make_pic_and_save(sentence, bing_cookie):
 def make_get_up_message(bing_cookie, up_list):
     sentence = get_one_sentence(up_list)
     now = pendulum.now(TIMEZONE)
-    # 3 - 8 means early for me
-    is_get_up_early = 3 <= now.hour <= 8
+    # 3 - 9 means early for me
+
+    is_get_up_early = 3 <= now.hour <= 24
+    # is_get_up_early = 3 <= now.hour <= 8
     get_up_time = now.to_datetime_string()
     link_list = []
     try:
@@ -133,7 +151,23 @@ def main(
         weather_message = f"现在的天气是{weather_message}\n"
         body = weather_message + early_message
     if is_get_up_early:
+        with open("knowledge.txt") as f:
+            all_my_knowledge_list = list(f.read().splitlines())
+        til_mds_list = get_all_til_knowledge_file()
+        file_name = None
+        while True:
+            file_name = random.choice(til_mds_list)
+            if file_name not in all_my_knowledge_list:
+                break
+        with open("knowledge.txt", "a") as f:
+            f.write(f"{file_name}\n")
         comment = body + f"![image]({link_for_issue})"
+        (
+            body
+            + f"\n\n今天的 TIL(github: {os.environ.get('MORNING_USER_NAME')/os.environ.get('MORNING_REPO_NAME')}): {file_name}"
+        )
+        with open(file_name) as f:
+            body += escape(f.read())
         issue.create_comment(comment)
         # send to telegram
         if tele_token and tele_chat_id:
