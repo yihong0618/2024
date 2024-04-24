a2422426fc909b74dee8c027dae6082d2284a8d8
yihong0618 <zouzou0208@gmail.com>
2024-04-24 21:59:17 +0800

    fix: title
    Signed-off-by: yihong0618 <zouzou0208@gmail.com>

--- get_up.py
+++ get_up.py
@@ -11,7 +11,7 @@ from openai import OpenAI
 import telegramify_markdown
 
 # 1 real get up #5 for test
-GET_UP_ISSUE_NUMBER = 5
+GET_UP_ISSUE_NUMBER = 1
 GET_UP_MESSAGE_TEMPLATE = "今天的起床时间是--{get_up_time}.\r\n\r\n 起床啦。\r\n\r\n 今天的一句诗:\r\n {sentence} \r\n"
 SENTENCE_API = "https://v1.jinrishici.com/all"
 POEM_API = "https://v2.jinrishici.com/sentence"
@@ -144,17 +144,17 @@ def main(
         weather_message = f"现在的天气是{weather_message}\n"
         body = weather_message + early_message
     if is_get_up_early:
-
         with open("knowledge.txt") as f:
             all_my_knowledge_list = list(f.read().splitlines())
         til_mds_list = get_all_til_knowledge_file()
         file_name = None
-        while True:
-            file_name = random.choice(til_mds_list)
-            if file_name not in all_my_knowledge_list:
-                break
-        with open("knowledge.txt", "a") as f:
-            f.write(f"{file_name}\n")
+        if til_mds_list:
+            while True:
+                file_name = random.choice(til_mds_list)
+                if file_name not in all_my_knowledge_list:
+                    break
+            with open("knowledge.txt", "a") as f:
+                f.write(f"{file_name}\n")
         comment = body + f"![image]({link_for_issue})"
         issue.create_comment(comment)
         # send to telegram
@@ -170,23 +170,24 @@ def main(
                     )
                 except:
                     pass
-            til_body = "TIL:\n"
-            user = os.environ.get("MORNING_USER_NAME")
-            repo = os.environ.get("MORNING_REPO_NAME")
-            branch = os.environ.get("MORNING_BRANCH_NAME")
-            link = f"https://github.com/{user}/{repo}/blob/{branch}/{'/'.join(file_name.split('/')[1:])}"
-            til_body = til_body + "Link: " + link + "\n"
-            with open(file_name) as f:
-                til_body = til_body + f.read()
-                if len(til_body) > 4095:
-                    til_body = til_body[:4094]
-                til_body = telegramify_markdown.convert(til_body)
-        bot.send_message(
-            tele_chat_id,
-            til_body,
-            parse_mode="MarkdownV2",
-            disable_notification=True,
-        )
+            if file_name:
+                til_body = "TIL:\n"
+                user = os.environ.get("MORNING_USER_NAME")
+                repo = os.environ.get("MORNING_REPO_NAME")
+                branch = os.environ.get("MORNING_BRANCH_NAME")
+                link = f"https://github.com/{user}/{repo}/blob/{branch}/{'/'.join(file_name.split('/')[1:])}"
+                til_body = til_body + "Link: " + link + "\n"
+                with open(file_name) as f:
+                    til_body = til_body + f.read()
+                    if len(til_body) > 4095:
+                        til_body = til_body[:4094]
+                    til_body = telegramify_markdown.convert(til_body)
+            bot.send_message(
+                tele_chat_id,
+                til_body,
+                parse_mode="MarkdownV2",
+                disable_notification=True,
+            )
     else:
         print("You wake up late")
 
