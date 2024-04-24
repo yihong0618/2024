f650cf9d452a3c5ab7a84333d0348e4931f31e5d
yihong0618 <zouzou0208@gmail.com>
2024-04-24 21:39:36 +0800

    fix: for test

--- TIL/README.md
+++
@@ -1,8 +0,0 @@
-This folder is to make my daily learning notes. 
-
-Followe this rules
-
-1. nanme -> xxxx-xx-xx.md
-2. content -> what I learned today in markdown format
-3. send to telegram every moring when I wake up
-4. if I don't have any notes, I will send a message with TIL
--- get_up.py
+++ get_up.py
@@ -11,7 +11,7 @@ from openai import OpenAI
 import telegramify_markdown
 
 # 1 real get up #5 for test
-GET_UP_ISSUE_NUMBER = 1
+GET_UP_ISSUE_NUMBER = 5
 GET_UP_MESSAGE_TEMPLATE = "今天的起床时间是--{get_up_time}.\r\n\r\n 起床啦。\r\n\r\n 今天的一句诗:\r\n {sentence} \r\n"
 SENTENCE_API = "https://v1.jinrishici.com/all"
 POEM_API = "https://v2.jinrishici.com/sentence"
@@ -26,10 +26,6 @@ else:
 SD_API_KEY = os.environ.get("SD_API_KEY")
 if not SD_API_KEY:
     raise Exception("you need get SD_API_KEY")
-
-
-def get_yesterday_md_file():
-    return f"{pendulum.yesterday(TIMEZONE).to_date_string()}.md"
 
 
 def get_all_til_knowledge_file():
@@ -100,47 +96,34 @@ def make_pic_and_save(sentence):
     new_path = os.path.join("OUT_DIR", date_str)
     if not os.path.exists(new_path):
         os.mkdir(new_path)
-    response = requests.post(
-        f"https://api.stability.ai/v2beta/stable-image/generate/sd3",
-        headers={"authorization": f"Bearer {SD_API_KEY}", "accept": "image/*"},
-        files={"none": ""},
-        data={
-            "prompt": prompt,
-            "model": "sd3-turbo",
-            "output_format": "jpeg",
-        },
+    response = client.images.generate(
+        model="dall-e-3", prompt=sentence, size="1024x1024", quality="standard", n=1
     )
-    index = 1
-    if response.status_code == 200:
-        with open(f"{new_path}/{index}.jpeg", "wb") as file:
-            file.write(response.content)
-    else:
-        print(str(response.json()))
-    image_url_for_issue = f"https://github.com/yihong0618/{year}/blob/main/OUT_DIR/{date_str}/{index}.jpeg?raw=true"
-    return image_url_for_issue, response.content
+    image_url_for_issue = response.model_dump()["data"][0]["url"]
+    return image_url_for_issue
 
 
 def make_get_up_message(up_list):
     sentence = get_one_sentence(up_list)
     now = pendulum.now(TIMEZONE)
     # 3 - 9 means early for me
-    is_get_up_early = 3 <= now.hour <= 7 
+    is_get_up_early = 3 <= now.hour <= 24 
     get_up_time = now.to_datetime_string()
     link_for_issue = ""
     try:
-        link_for_issue, content = make_pic_and_save(sentence)
+        link_for_issue = make_pic_and_save(sentence)
     except Exception as e:
         print(str(e))
         # give it a second chance
         try:
             sentence = get_one_sentence(up_list)
             print(f"Second: {sentence}")
-            link_for_issue, content = make_pic_and_save(sentence)
+            link_for_issue = make_pic_and_save(sentence)
         except Exception as e:
             print(str(e))
     body = GET_UP_MESSAGE_TEMPLATE.format(get_up_time=get_up_time, sentence=sentence)
     print(body, link_for_issue)
-    return body, is_get_up_early, link_for_issue, content
+    return body, is_get_up_early, link_for_issue
 
 
 def main(
@@ -157,9 +140,7 @@ def main(
     if is_today:
         print("Today I have recorded the wake up time")
         return
-    early_message, is_get_up_early, link_for_issue, content = make_get_up_message(
-        up_list
-    )
+    early_message, is_get_up_early, link_for_issue = make_get_up_message(up_list)
     body = early_message
     if weather_message:
         weather_message = f"现在的天气是{weather_message}\n"
@@ -184,21 +165,18 @@ def main(
             if link_for_issue:
                 try:
                     bot.send_photo(
-                        tele_chat_id, content, caption=body, disable_notification=True
+                        tele_chat_id,
+                        link_for_issue,
+                        caption=body,
+                        disable_notification=True,
                     )
                 except:
                     pass
-            # my file and other files
-            yseterday_md = get_yesterday_md_file()
             til_body = "TIL:\n"
-            if os.path.exists(f"TIL/{get_yesterday_md_file}"):
-                link = f"https://github.com/yihong0618/2024/blob/main/TIL/{get_yesterday_md_file}"
-                file_name = yseterday_md
-            else:
-                user = os.environ.get("MORNING_USER_NAME")
-                repo = os.environ.get("MORNING_REPO_NAME")
-                branch = os.environ.get("MORNING_BRANCH_NAME")
-                link = f"https://github.com/{user}/{repo}/blob/{branch}/{'/'.join(file_name.split('/')[1:])}"
+            user = os.environ.get("MORNING_USER_NAME")
+            repo = os.environ.get("MORNING_REPO_NAME")
+            branch = os.environ.get("MORNING_BRANCH_NAME")
+            link = f"https://github.com/{user}/{repo}/blob/{branch}/{'/'.join(file_name.split('/')[1:])}"
             til_body = til_body + "Link: " + link + "\n"
             with open(file_name) as f:
                 til_body = til_body + f.read()
