7609c66fef2a086a9c5777414d4780b0a9e87d4b
yihong0618 <zouzou0208@gmail.com>
2024-06-17 11:51:55 +0800

    fix

--- get_up.py
+++ get_up.py
@@ -12,7 +12,7 @@ import telegramify_markdown
 
 # 1 real get up #5 for test
 GET_UP_ISSUE_NUMBER = 1
-GET_UP_MESSAGE_TEMPLATE = "今天的起床时间是--{get_up_time}.\r\n\r\n 起床啦。\r\n\r\n 今天的一句诗:\r\n {sentence} \r\n"
+GET_UP_MESSAGE_TEMPLATE = "今天的起床时间是--{get_up_time}.\r\n\r\n 起床啦。\r\n\r\n 今天的一句诗:\r\n{sentence}\r\n"
 # in 2024-06-15 this one ssl error
 SENTENCE_API = "https://v1.jinrishici.com/all"
 SENTENCE_API = (
