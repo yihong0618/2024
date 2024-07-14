fd660d6d80e455b6d82f8326cdba991afaa90ba9
yihong0618 <zouzou0208@gmail.com>
2024-07-14 23:46:19 +0800

    feat: use kling
    Signed-off-by: yihong0618 <zouzou0208@gmail.com>

--- get_up.py
+++ get_up.py
@@ -6,18 +6,17 @@ import random
 import pendulum
 import requests
 import telebot
+from telebot.types import InputMediaPhoto, InputMediaVideo
 from github import Github
 from openai import OpenAI
-import telegramify_markdown
+from kling import VideoGen, ImageGen
 
 # 1 real get up #5 for test
-GET_UP_ISSUE_NUMBER = 1
+GET_UP_ISSUE_NUMBER = 5
 GET_UP_MESSAGE_TEMPLATE = "今天的起床时间是--{get_up_time}.\r\n\r\n 起床啦。\r\n\r\n 今天的一句诗:\r\n{sentence}\r\n"
 # in 2024-06-15 this one ssl error
 SENTENCE_API = "https://v1.jinrishici.com/all"
-SENTENCE_API = (
-    "https://api.sou-yun.cn/api/RecommendedReading?type=poem&needHtml=true&index=-1"
-)
+
 DEFAULT_SENTENCE = (
     "赏花归去马如飞\r\n去马如飞酒力微\r\n酒力微醒时已暮\r\n醒时已暮赏花归\r\n"
 )
@@ -26,6 +25,8 @@ if api_base := os.environ.get("OPENAI_API_BASE"):
     client = OpenAI(base_url=api_base, api_key=os.environ.get("OPENAI_API_KEY"))
 else:
     client = OpenAI()
+
+KLING_COOKIE = os.environ.get("KLING_COOKIE")
 
 
 def get_all_til_knowledge_file():
@@ -43,15 +44,11 @@ def login(token):
     return Github(token)
 
 
-def get_one_sentence(up_list):
+def get_one_sentence():
     try:
         r = requests.get(SENTENCE_API)
         if r.ok:
-            content = ""
-            concent_list = r.json()["Content"]["Poem"]["Clauses"]
-            for c in concent_list:
-                content += c["Content"] + "\r\n"
-            return content
+            return r.json()["content"]
         return DEFAULT_SENTENCE
     except:
         print("get SENTENCE_API wrong")
@@ -62,21 +59,13 @@ def get_today_get_up_status(issue):
     comments = list(issue.get_comments())
     if not comments:
         return False, []
-    up_list = []
-    for comment in comments:
-        try:
-            s = comment.body.splitlines()[6]
-            up_list.append(s)
-        except Exception as e:
-            print(str(e), "!!")
-            continue
     latest_comment = comments[-1]
     now = pendulum.now(TIMEZONE)
     latest_day = pendulum.instance(latest_comment.created_at).in_timezone(
         "Asia/Shanghai"
     )
     is_today = (latest_day.day == now.day) and (latest_day.month == now.month)
-    return is_today, up_list
+    return is_today
 
 
 def make_pic_and_save(sentence):
@@ -96,49 +85,39 @@ def make_pic_and_save(sentence):
     new_path = os.path.join("OUT_DIR", date_str)
     if not os.path.exists(new_path):
         os.mkdir(new_path)
-    response = client.images.generate(
-        model="dall-e-3", prompt=sentence, size="1024x1024", quality="standard", n=1
-    )
-    image_url_for_issue = response.model_dump()["data"][0]["url"]
-    response = requests.get(image_url_for_issue)
-    with open(f"{new_path}/1.png", "wb") as f:
-        f.write(response.content)
+    # response = client.images.generate(
+    #     model="dall-e-3", prompt=sentence, size="1024x1024", quality="standard", n=1
+    # )
+    # image_url_for_issue = response.model_dump()["data"][0]["url"]
+    # response = requests.get(image_url_for_issue)
+    # with open(f"{new_path}/1.png", "wb") as f:
+    #     f.write(response.content)
     # try to use this to generate video
-    try:
-        from luma import VideoGen
-
-        cookie = os.environ.get("LUMA_COOKIE")
-        v = VideoGen(cookie, f"{new_path}/1.png")
-        video_path = v.save_video(sentence, new_path)
-        return image_url_for_issue, video_path
-    except Exception as e:
-        print("No luma")
-        print(str(e))
-    return image_url_for_issue, None
+    i = ImageGen(KLING_COOKIE)
+    images_list = i.get_images(sentence)
+    return images_list
 
 
-def make_get_up_message(up_list):
-    sentence = get_one_sentence(up_list)
+def make_get_up_message():
+    sentence = get_one_sentence()
     now = pendulum.now(TIMEZONE)
     # 3 - 7 means early for me
-    is_get_up_early = 3 <= now.hour <= 7
+    is_get_up_early = 3 <= now.hour <= 24
     get_up_time = now.to_datetime_string()
-    link_for_issue = ""
-    video_path = None
     try:
-        link_for_issue, video_path = make_pic_and_save(sentence)
+        images_list = make_pic_and_save(sentence)
     except Exception as e:
         print(str(e))
         # give it a second chance
         try:
-            sentence = get_one_sentence(up_list)
+            sentence = get_one_sentence()
             print(f"Second: {sentence}")
-            link_for_issue, video_path = make_pic_and_save(sentence)
+            images_list = make_pic_and_save(sentence)
         except Exception as e:
             print(str(e))
     body = GET_UP_MESSAGE_TEMPLATE.format(get_up_time=get_up_time, sentence=sentence)
-    print(body, link_for_issue, video_path)
-    return body, is_get_up_early, link_for_issue, video_path
+    print(body, images_list)
+    return body, is_get_up_early, images_list
 
 
 def main(
@@ -151,52 +130,38 @@ def main(
     u = login(github_token)
     repo = u.get_repo(repo_name)
     issue = repo.get_issue(GET_UP_ISSUE_NUMBER)
-    is_today, up_list = get_today_get_up_status(issue)
+    is_today = get_today_get_up_status(issue)
     if is_today:
         print("Today I have recorded the wake up time")
         return
-    early_message, is_get_up_early, link_for_issue, video_path = make_get_up_message(
-        up_list
-    )
+    early_message, is_get_up_early, images_list = make_get_up_message()
     body = early_message
     if weather_message:
         weather_message = f"现在的天气是{weather_message}\n"
         body = weather_message + early_message
     if is_get_up_early:
-        # with open("knowledge.txt") as f:
-        #     all_my_knowledge_list = list(f.read().splitlines())
-        # til_mds_list = get_all_til_knowledge_file()
-        # file_name = None
-        # if til_mds_list:
-        #     while True:
-        #         file_name = random.choice(til_mds_list)
-        #         if file_name not in all_my_knowledge_list:
-        #             break
-        #     with open("knowledge.txt", "a") as f:
-        #         f.write(f"{file_name}\n")
-        comment = body + f"![image]({link_for_issue})"
+        comment = body + f"![image]({images_list[0]})"
         issue.create_comment(comment)
         # send to telegram
         if tele_token and tele_chat_id:
             bot = telebot.TeleBot(tele_token)
-            if link_for_issue:
-                if video_path:
-                    bot.send_video(
-                        tele_chat_id,
-                        open(video_path, "rb"),
-                        caption=body,
-                        disable_notification=True,
+            if images_list:
+                try:
+                    photos_list = [InputMediaPhoto(i) for i in images_list[:4]]
+                    photos_list[0].caption = body
+                    bot.send_media_group(
+                        tele_chat_id, photos_list, disable_notification=True
                     )
-                else:
-                    try:
-                        bot.send_photo(
-                            tele_chat_id,
-                            link_for_issue,
-                            caption=body,
-                            disable_notification=True,
-                        )
-                    except:
-                        pass
+                except Exception as e:
+                    print(str(e))
+                v = VideoGen(KLING_COOKIE)
+                v.save_video("让画面动起来", "./output", image_url=images_list[0])
+                bot.send_video(
+                    tele_chat_id,
+                    open("output/0.mp4", "rb"),
+                    caption=body,
+                    disable_notification=True,
+                )
     else:
         print("You wake up late")
 
